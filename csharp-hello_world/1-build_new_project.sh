650bb7e4e9231b8bc274b1ec9675dd1970742a0e
Romina Guimaraens <5071@holbertonstudents.com>
2023-07-25 22:36:00 -0300

    Initial commit

--- csharp-hello_world/1-build_new_project.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir 1-new_project
-cd 1-new_project
-dotnet new console
-dotnet build
--- csharp-hello_world/2-run_new_project.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir 2-new_project
-cd 2-new_project
-dotnet new console
-dotnet build
-dotnet run
