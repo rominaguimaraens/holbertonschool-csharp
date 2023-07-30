c77449d4a99397478d96fb4fc1903b2f7eabeed4
Romina Guimaraens <5071@holbertonstudents.com>
2023-07-29 23:24:24 -0300

    task 7

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -6,7 +6,7 @@ class Program
     {
         string str1 = "Holberton";
         string str2 = "School";
-        str1 = string.Concat(str1, " ", str2)
+        string str1 = string.Concat(str1, " ", str2)
         Console.WriteLine("Welcome to {0}!", str1);
     }
 }
