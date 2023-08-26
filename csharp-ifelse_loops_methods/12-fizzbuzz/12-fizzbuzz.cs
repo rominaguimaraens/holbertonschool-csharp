0bb06abfe6e1c3f676700f77c3b5f0b762fdca16
Romina Guimaraens <5071@holbertonstudents.com>
2023-08-26 20:29:06 -0300

    task 10 and 11

--- csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace _12_fizzbuzz
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        for (int i = 1; i <= 100; i++)
         {
-            Console.WriteLine("Hello World!");
+            if ((i % 3 == 0) && (i % 5 == 0))
+            {
+                Console.Write("FizzBuzz ");
+                continue;
+            } else if (i % 3 == 0) {
+                Console.Write("Fizz ");
+                continue;
+            } else if (i % 5 == 0) {
+                Console.Write("Buzz ");
+                continue;
+            }
+            Console.Write("{0} ", i);
         }
     }
 }
