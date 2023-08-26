b5d924f3306e71752d9d36d075e5d62ab7a0e0a5
Romina Guimaraens <5071@holbertonstudents.com>
2023-08-26 20:15:16 -0300

    task 6

--- csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -6,7 +6,19 @@ namespace _6_print_comb2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i, j;
+            for (i = 0; i < 9; i++)
+            {
+                for (j = i + 1; j < 10; j++)
+                {
+                    if (i == 8 && j == 9)
+                    {
+                        Console.WriteLine("89");
+                        return;
+                    }
+                    Console.Write("{0}{1}, ", i, j);
+                }
+            }
         }
     }
 }
